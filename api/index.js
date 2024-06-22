8b5aa76f16663c0c12c41abc9d07b1bf211f59a3
ikim319 <ikim319@gatech.edu>
2024-06-22 15:33:59 -0400

    added functionality for navigation to index page upon login

--- api/index.js
+++ api/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('./models/User.js');
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 
 const bcryptSalt = bcrypt.genSaltSync(10);
+const jwtSecret = 'fdiopquhoieuhfijkdsj2341';
 
 app.use(express.json());
 app.use(cors({
@@ -23,13 +25,35 @@ app.get('/test', (req,res) => {
 
 app.post('/register', async (req,res) => {
     const {name,email,password} = req.body;
-    const userDoc = await User.create({
-        name,
-        email,
-        password:bcrypt.hashSync(password, bcryptSalt),
-    });
+    try {
+        const userDoc = await User.create({
+            name,
+            email,
+            password:bcrypt.hashSync(password, bcryptSalt),
+        });
 
-    res.json(userDoc);
+        res.json(userDoc);
+    } catch (e) {
+        res.status(422).json(e); //http statuses, 422 -> unprocessable entity
+    }
+});
+
+app.post('/login', async (req, res) => {
+    const {email,password} = req.body;
+    const userDoc = await User.findOne({email});
+    if (userDoc) {
+        const passOk = bcrypt.compareSync(password, userDoc.password);
+        if (passOk) {
+            jwt.sign({email:userDoc.email, id:userDoc._id}, jwtSecret, {}, (err,token) => {
+                if (err) throw err;
+                res.cookie('token', token).json('pass ok');
+            });
+        } else {
+            res.status(422).json('pass not ok');
+        }
+    } else {
+        res.json('not found');
+    }
 });
 
 app.listen(4000);
