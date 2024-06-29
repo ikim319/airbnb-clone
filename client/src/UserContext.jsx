1f4df777174386beb5ac12a0585440b8681f0dbf
ikim319 <114134560+ikim319@users.noreply.github.com>
2024-06-29 19:56:06 -0400

    added logout functionality and made progress on layout for account page

--- client/src/UserContext.jsx
+++ client/src/UserContext.jsx
@@ -5,16 +5,17 @@ export const UserContext = createContext({});
 
 export function UserContextProvider({children}) {
     const [user, setUser] = useState(null);
+    const [ready, setReady] = useState(false)
     useEffect(() => {
         if (!user) {
-            const {data} = axios.get('/profile').then(({data}) => {
+            axios.get('/profile').then(({data}) => {
                 setUser(data);
+                setReady(true);
             });
-            setUser(data);
         }
     }, []);
     return (
-        <UserContext.Provider value={{user,setUser}}>
+        <UserContext.Provider value={{user,setUser,ready}}>
             {children}
         </UserContext.Provider> 
     );
