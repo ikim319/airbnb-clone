a6d7f6425fb8725d8fbc411838607f136388fa10
ikim319 <114134560+ikim319@users.noreply.github.com>
2024-07-15 21:35:24 -0400

    initialized bookings page and single booking page, established association of each booking to user

--- api/index.js
+++ api/index.js
@@ -27,6 +27,15 @@ app.use(cors({
 // console.log(process.env.MONGO_URL);
 mongoose.connect(process.env.MONGO_URL);
 
+function getUserDataFromReq(req) {
+    return new Promise((resolve,reject) => {
+        jwt.verify(req.cookies.token, jwtSecret, {}, async (err, userData) => {
+            if (err) throw err;
+            resolve(userData);
+        });
+    });
+}
+
 app.get('/test', (req,res) => {
     res.json('test ok');
 });
@@ -165,17 +174,24 @@ app.get('/places', async (req,res) => {
     res.json( await Place.find() )
 });
 
-app.post('/bookings', (req,res) => {
+app.post('/bookings', async (req,res) => {
+    const userData = await getUserDataFromReq(req);
     const {
         place,checkIn,checkOut,numberOfGuests,name,phone,price
     } = req.body;
     Booking.create({
-        place,checkIn,checkOut,numberOfGuests,name,phone,price
+        place,checkIn,checkOut,numberOfGuests,name,phone,price,
+        user:userData.id,
     }).then((doc) => {
         res.json(doc);
     }).catch((err) => {
         throw err;
     })
+});
+
+app.get('/bookings', async (req,res) => {
+    const userData = await getUserDataFromReq(req);
+    res.json( await Booking.find({user:userData.id}) )
 });
 
 app.listen(4000);
---
+++ client/src/pages/BookingsPage.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react";
+import AccountNav from "./AccountNav";
+import axios from "axios";
+
+export default function BookingsPage() {
+    const [bookings,setBookings] = useState([]);
+    useEffect(() => {
+        axios.get('/bookings').then(response => {
+            setBookings(response.data);
+        });
+    }, []);
+    return (
+        <div>
+            <AccountNav />
+            <div>
+                {bookings?.length > 0 && bookings.map(booking => (
+                    <div>
+                        {booking.checkIn} -> {booking.checkOut}
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+}
